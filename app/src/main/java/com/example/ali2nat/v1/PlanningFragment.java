2c5b818de7827c4ce4b5ed120b15b346dd6ea254
Ryxar <adrien.duvent@epfedu.fr>
2016-05-24 17:05:41 +0100

    PlanningFragment Update
    ajout de l'open-source du planning en fragment

--- app/src/main/java/com/example/ali2nat/v1/PlanningFragment.java
+++ app/src/main/java/com/example/ali2nat/v1/PlanningFragment.java
@@ -1,29 +1,138 @@
 package com.example.ali2nat.v1;
 
 
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PlanningFragment extends Fragment {
+public class PlanningFragment extends AbstractFragment   {
+
+        public List<? extends WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+            // Populate the week view with some events.
+            List<WeekViewEvent> events = new ArrayList<WeekViewEvent>();
+
+            Calendar startTime = Calendar.getInstance();
+            startTime.set(Calendar.HOUR_OF_DAY, 3);
+            startTime.set(Calendar.MINUTE, 0);
+            startTime.set(Calendar.MONTH, newMonth - 1);
+            startTime.set(Calendar.YEAR, newYear);
+            Calendar endTime = (Calendar) startTime.clone();
+            endTime.add(Calendar.HOUR, 1);
+            endTime.set(Calendar.MONTH, newMonth - 1);
+            WeekViewEvent event = new WeekViewEvent(1, getEventTitle(startTime), startTime, endTime);
+            event.setColor(getResources().getColor(R.color.event_color_01));
+            events.add(event);
+
+            startTime = Calendar.getInstance();
+            startTime.set(Calendar.HOUR_OF_DAY, 3);
+            startTime.set(Calendar.MINUTE, 30);
+            startTime.set(Calendar.MONTH, newMonth-1);
+            startTime.set(Calendar.YEAR, newYear);
+            endTime = (Calendar) startTime.clone();
+            endTime.set(Calendar.HOUR_OF_DAY, 4);
+            endTime.set(Calendar.MINUTE, 30);
+            endTime.set(Calendar.MONTH, newMonth-1);
+            event = new WeekViewEvent(10, getEventTitle(startTime), startTime, endTime);
+            event.setColor(getResources().getColor(R.color.event_color_02));
+            events.add(event);
+
+            startTime = Calendar.getInstance();
+            startTime.set(Calendar.HOUR_OF_DAY, 4);
+            startTime.set(Calendar.MINUTE, 20);
+            startTime.set(Calendar.MONTH, newMonth-1);
+            startTime.set(Calendar.YEAR, newYear);
+            endTime = (Calendar) startTime.clone();
+            endTime.set(Calendar.HOUR_OF_DAY, 5);
+            endTime.set(Calendar.MINUTE, 0);
+            event = new WeekViewEvent(10, getEventTitle(startTime), startTime, endTime);
+            event.setColor(getResources().getColor(R.color.event_color_03));
+            events.add(event);
+
+            startTime = Calendar.getInstance();
+            startTime.set(Calendar.HOUR_OF_DAY, 5);
+            startTime.set(Calendar.MINUTE, 30);
+            startTime.set(Calendar.MONTH, newMonth-1);
+            startTime.set(Calendar.YEAR, newYear);
+            endTime = (Calendar) startTime.clone();
+            endTime.add(Calendar.HOUR_OF_DAY, 2);
+            endTime.set(Calendar.MONTH, newMonth-1);
+            event = new WeekViewEvent(2, getEventTitle(startTime), startTime, endTime);
+            event.setColor(getResources().getColor(R.color.event_color_02));
+            events.add(event);
+
+            startTime = Calendar.getInstance();
+            startTime.set(Calendar.HOUR_OF_DAY, 5);
+            startTime.set(Calendar.MINUTE, 0);
+            startTime.set(Calendar.MONTH, newMonth - 1);
+            startTime.set(Calendar.YEAR, newYear);
+            startTime.add(Calendar.DATE, 1);
+            endTime = (Calendar) startTime.clone();
+            endTime.add(Calendar.HOUR_OF_DAY, 3);
+            endTime.set(Calendar.MONTH, newMonth - 1);
+            event = new WeekViewEvent(3, getEventTitle(startTime), startTime, endTime);
+            event.setColor(getResources().getColor(R.color.event_color_03));
+            events.add(event);
+
+            startTime = Calendar.getInstance();
+            startTime.set(Calendar.DAY_OF_MONTH, 15);
+            startTime.set(Calendar.HOUR_OF_DAY, 3);
+            startTime.set(Calendar.MINUTE, 0);
+            startTime.set(Calendar.MONTH, newMonth-1);
+            startTime.set(Calendar.YEAR, newYear);
+            endTime = (Calendar) startTime.clone();
+            endTime.add(Calendar.HOUR_OF_DAY, 3);
+            event = new WeekViewEvent(4, getEventTitle(startTime), startTime, endTime);
+            event.setColor(getResources().getColor(R.color.event_color_04));
+            events.add(event);
+
+            startTime = Calendar.getInstance();
+            startTime.set(Calendar.DAY_OF_MONTH, 1);
+            startTime.set(Calendar.HOUR_OF_DAY, 3);
+            startTime.set(Calendar.MINUTE, 0);
+            startTime.set(Calendar.MONTH, newMonth-1);
+            startTime.set(Calendar.YEAR, newYear);
+            endTime = (Calendar) startTime.clone();
+            endTime.add(Calendar.HOUR_OF_DAY, 3);
+            event = new WeekViewEvent(5, getEventTitle(startTime), startTime, endTime);
+            event.setColor(getResources().getColor(R.color.event_color_01));
+            events.add(event);
+
+            startTime = Calendar.getInstance();
+            startTime.set(Calendar.DAY_OF_MONTH, startTime.getActualMaximum(Calendar.DAY_OF_MONTH));
+            startTime.set(Calendar.HOUR_OF_DAY, 15);
+            startTime.set(Calendar.MINUTE, 0);
+            startTime.set(Calendar.MONTH, newMonth-1);
+            startTime.set(Calendar.YEAR, newYear);
+            endTime = (Calendar) startTime.clone();
+            endTime.add(Calendar.HOUR_OF_DAY, 3);
+            event = new WeekViewEvent(5, getEventTitle(startTime), startTime, endTime);
+            event.setColor(getResources().getColor(R.color.event_color_02));
+            events.add(event);
 
 
-    public PlanningFragment() {
-        // Required empty public constructor
+
+            return events;
+        }
+
     }
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_planning, container, false);
-    }
 
-}
+
+
+
